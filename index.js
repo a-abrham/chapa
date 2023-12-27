fd2129f270769d11c2f4efd50527c60644f7ddbf
abrshkorra <abrshkorra@gmail.com>
2023-12-27 13:11:27 +0300

    included random num in the tx ref

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ function setTxRef() {
     var firstName = document.getElementById('firstName').value;
     var lastName = document.getElementById('lastName').value;
     var currentDate = new Date().toISOString().slice(0, 10).replace(/-/g, '');
+    var randomNum = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
 
-    var txRef = firstName + '-' + lastName + '-' + currentDate;
+
+    var txRef = firstName + '-' + lastName + '-' + currentDate + '-' + randomNum;
     document.getElementById('txRefHidden').value = txRef;
 }
